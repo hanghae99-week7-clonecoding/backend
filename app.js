99eb511a5982010c80a47be4423182f1cceacb73
hinel03 <Sung.may4th@hotmail.com>
2022-08-22 20:32:41 +0900

    updated:cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const Router = require("./routes/index");
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
 
@@ -7,6 +8,13 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use("/", Router);
+
+app.use(
+  cors({
+  credentials: true,
+  origin: "http://localhost:3000",
+  })
+  );
 
 app.get("/", (req, res) => {
     res.status(200).json({ message :"메인화면" })

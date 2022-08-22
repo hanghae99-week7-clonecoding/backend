e1f861e94614c38bf6b48bdf7bcc4a544027be3d
hinel03 <Sung.may4th@hotmail.com>
2022-08-22 21:32:01 +0900

    fixed: app

--- app.js
+++ app.js
@@ -9,13 +9,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/", Router);
 
-app.use(
-  cors({
-  credentials: true,
-  origin: true,
-  })
-  );
-
+app.use(cors('*'));
+  
 app.get("/", (req, res) => {
     res.status(200).json({ message :"메인화면" })
 })

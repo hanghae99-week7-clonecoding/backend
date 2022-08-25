2c1f4ffe12578ea3ad014a95ca634ff61160a8d9
winmin90 <108992367+winmin90@users.noreply.github.com>
2022-08-25 12:38:55 +0900

    구독 부분

--- routes/subscribes.js
+++ routes/subscribes.js
@@ -18,37 +18,11 @@ router.post("/:postId", authMiddleware, async(req, res) => {
       if(!postToSubcribe){
         await User.update({ subscribe: postToSubcribe +1 }, { where: { channel,userId } });
         await Subs.create({ channel,userId });             
-        return res.status(200).json({ 
-          message: "구독이 추가되었습니다." ,
-          result: {
-            postId: data.postId,
-            title: data.title,
-            category: data.category,
-            discription: data.discription,
-            url: data.url,
-            like: data.like,
-            channel: data.channel,
-            userimage: data.userimage,
-            subscribe: "구독자"
-        }
-      });
+        return res.status(200).json({ message: "구독이 추가되었습니다.", result:"구독자" });
       }else{
         await User.update({ subscribe: postToSubcribe -1 }, { where: { channel,userId } });
         await Subs.destroy({ where: { channel:channel, userId:userId } });             
-        return res.status(200).json({ 
-          message: "구독정보가 삭제되었습니다.",
-          result: {
-            postId: data.postId,
-            title: data.title,
-            category: data.category,
-            discription: data.discription,
-            url: data.url,
-            like: data.like,
-            channel: data.channel,
-            userimage: data.userimage,
-            subscribe: "비구독자"
-        }
-         });
+        return res.status(200).json({ message: "구독정보가 삭제되었습니다.", result:"비구독자" });
       } 
   }catch(error){
   const message = `${req.method} ${req.originalUrl} : ${error.message}`;
